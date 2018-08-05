3ffc2f58a8e3eeeeac7ea9131532e91822748606
Eugenia <eugenia.kaloshina@gmail.com>
2018-08-05 00:36:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 var kittens = ["Milo","Otis","Garfield"]
 
 function destructivelyAppendKitten(name) {
-  kittens.push(kitten)
+  kittens.push("kitten")
   return kittens
 }
 
 function destructivelyPrependKitten(name)  {
-  kittens.unshift(kitten)
+  kittens.unshift("kitten")
   return kitten
 }
