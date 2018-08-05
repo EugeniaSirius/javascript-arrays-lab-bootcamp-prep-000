b9c57fc8991655b97aaa1eca534cd03895dea771
Eugenia <eugenia.kaloshina@gmail.com>
2018-08-05 00:34:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Add your functions and code here
+var kittens = ["Milo","Otis","Garfield"]
+
+function destructivelyAppendKitten(name) {
+  kittens.push(kitten)
+  return kittens
+}
+
+function destructivelyPrependKitten(name)  {
+  kittens.unshift(kitten)
+  return kitten
+}
